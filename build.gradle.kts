c89e21f99696fb8ba473630ae63a44dea85de724
GitHub Action <action@github.com>
2026-04-23 08:13:43 +0000

    Bump version to 3.1.19 (301019)

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2.11.27"
-            versionCode = 211027
+            versionName = "3.1.19"
+            versionCode = 301019
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
