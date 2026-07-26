c3fbeee6ef71a215e36ba13f0b014054e5f966ab
GitHub Action <action@github.com>
2026-07-26 12:16:35 +0000

    Bump version to 3.1.24 (301024)

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "3.1.23"
-            versionCode = 301023
+            versionName = "3.1.24"
+            versionCode = 301024
             versionName = "2.11.32"
             versionCode = 211032
 
